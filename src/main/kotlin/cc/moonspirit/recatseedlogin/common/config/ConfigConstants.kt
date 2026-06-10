3d546eda451f60fe47c9e0a8d9558ba3ce7ed48a
sanjiu2024 <3375927974@qq.com>
2026-06-10 12:45:07 +0800

    docs: add copyright headers to all 64 Kotlin source files
    Prepend each .kt file with attribution header acknowledging:
    - Original work: CatSeedLogin, MIT License (2021, CatSeed)
    - Modifications: GPL v3.0-or-later (2026, Yueling)
    
    Header format:
    /*
     * Original work: CatSeedLogin
     * Copyright (c) 2021 CatSeed
     *
     * Licensed under the MIT License - see the LICENSE file for details.
     * (Original package: cc.baka9.catseedlogin)
     *
     * -------------------------------------------------
     * Modifications and additional code
     * Copyright (c) 2026 Yueling
     * This work is licensed under the GNU GPL v3.0-or-later
     */

--- src/main/kotlin/cc/moonspirit/recatseedlogin/common/config/ConfigConstants.kt
+++ src/main/kotlin/cc/moonspirit/recatseedlogin/common/config/ConfigConstants.kt
@@ -1,3 +1,16 @@
+/*
+ * Original work: CatSeedLogin
+ * Copyright (c) 2021 CatSeed
+ *
+ * Licensed under the MIT License - see the LICENSE file for details.
+ * (Original package: cc.baka9.catseedlogin)
+ *
+ * -------------------------------------------------
+ * Modifications and additional code
+ * Copyright (c) 2026 Yueling
+ * This work is licensed under the GNU GPL v3.0-or-later
+ */
+
 package cc.moonspirit.recatseedlogin.common.config
 
 import java.util.regex.Pattern
